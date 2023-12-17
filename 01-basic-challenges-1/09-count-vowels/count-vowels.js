3f8245f4ea6bd29266ab707e965d04441ed0c8fc
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-17 11:56:50 +0530

    new commit🚀

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,30 @@
-function countVowels() {}
+// function countVowels(str) {
+//   let count = 0;
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   for (let char of str.toLowerCase()) {
+//     if (vowels.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// Solution 2
+function countVowels(str) {
+  const formattedStr = str.toLowerCase();
+  let count = 0;
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    if (
+      formattedStr[i] === "a" ||
+      formattedStr[i] === "e" ||
+      formattedStr[i] === "i" ||
+      formattedStr[i] === "o" ||
+      formattedStr[i] === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,24 @@
-function removeDuplicates() {}
+// function removeDuplicates(arr) {
+//   const uniqueArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (uniqueArr.indexOf(arr[i]) === -1) {
+//       uniqueArr.push(arr[i]);
+//     }
+//   }
+//   return uniqueArr;
+// }
+
+// Solution 2
+function removeDuplicates(arr) {
+  const uniqueArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArr.includes(arr[i])) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+  return uniqueArr;
+}
 
 module.exports = removeDuplicates;
