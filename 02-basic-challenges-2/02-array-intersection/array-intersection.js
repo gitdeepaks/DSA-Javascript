e03ea6352abc288aef33341ea569e398c66e7f27
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-19 20:47:32 +0530

    new commit🚀

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,24 @@
-function arrayIntersection() {}
+// function arrayIntersection(arr1, arr2) {
+//   const intersection = [];
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     const current = arr1[i];
+//     if (arr2.includes(current)) {
+//       intersection.push(current);
+//     }
+//   }
+//   return intersection;
+// }
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+
+  const intersectionArr = [];
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      intersectionArr.push(num);
+    }
+  }
+  return intersectionArr;
+}
 
 module.exports = arrayIntersection;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.test.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.test.js
@@ -1,6 +1,6 @@
-const arrayIntersection = require('./array-intersection');
+const arrayIntersection = require("./array-intersection");
 
-test('Finding array intersection', () => {
+test("Finding array intersection", () => {
   expect(arrayIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7])).toEqual([
     3, 4, 5,
   ]);
