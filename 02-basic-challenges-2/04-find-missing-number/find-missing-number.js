1b79caf90def58b7b77f50587f1e9328a16a8449
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-20 21:55:52 +0530

    new commit🚀

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,26 @@
-function findMissingNumber() {}
+// function findMissingNumber(arr) {
+//   if (arr.length === 0) {
+//     return 1;
+//   }
+
+//   const n = arr.length + 1;
+//   const expectedSum = (n * (n + 1)) / 2;
+//   const actualSum = arr.reduce((a, b) => a + b);
+//   return expectedSum - actualSum;
+// }
+function findMissingNumber(arr) {
+  if (arr.length === 0) {
+    return 1;
+  }
+
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    actualSum += arr[i];
+  }
+
+  return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,22 @@
-function findMissingLetter() {}
+// function findMissingLetter(arr) {
+//   const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   const startIndex = alphabet.indexOf(arr[0]);
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== alphabet[startIndex + i]) {
+//       return alphabet[startIndex + i];
+//     }
+//   }
+//   return "";
+// }
+function findMissingLetter(arr) {
+  let start = arr[0].charCodeAt(0);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].charCodeAt(0) !== start + i) {
+      return String.fromCharCode(start + i);
+    }
+  }
+  return "";
+}
 
 module.exports = findMissingLetter;
