7ebe33e503394f7224b81339149698859ef3bdee
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-21 15:54:52 +0530

    new commit🚀

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,12 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  let chars = {};
+  for (let i = 0; i < str.length; i++) {
+    if (chars[str[i]]) {
+      return false;
+    }
+    chars[str[i]] = true;
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
