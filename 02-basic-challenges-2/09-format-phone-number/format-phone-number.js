c544d2cace2ff11ee3a6ebb8a285627d70096daf
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-29 11:02:07 +0530

    new commit🚀

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,14 @@
-function formatPhoneNumber() {}
+// function formatPhoneNumber(numbers) {
+//   const aearCode = numbers.slice(0, 3).join("");
+//   const preFix = numbers.slice(3, 6).join("");
+//   const lineNumber = numbers.slice(6).join("");
+//   return `(${aearCode}) ${preFix}-${lineNumber}`;
+// }
+function formatPhoneNumber(numbers) {
+  const formatted = numbers.join("");
+  return `(${formatted.slice(0, 3)}) ${formatted.slice(3, 6)}-${formatted.slice(
+    6
+  )}`;
+}
 
 module.exports = formatPhoneNumber;
