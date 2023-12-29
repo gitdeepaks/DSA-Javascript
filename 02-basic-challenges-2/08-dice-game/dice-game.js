1fc84263c882cd909e5e93f0293b47ffb41c007f
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-29 10:16:16 +0530

    new commit🚀

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,28 @@
-function diceGameSimulation() {}
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function diceGameSimulation(numSim) {
+  const res = [];
+
+  for (let i = 0; i < numSim; i++) {
+    const dice1 = rollDice();
+    const dice2 = rollDice();
+    const sum = dice1 + dice2;
+
+    let res = "";
+
+    if (sum === 7 || sum === 11) {
+      res = "win";
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      res = "lose";
+    } else {
+      res = "roll again";
+    }
+    res.push({ dice1, dice2, sum, res });
+  }
+
+  return res;
+}
 
 module.exports = diceGameSimulation;
