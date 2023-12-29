53a6e198464948b923ff00c51206c189e5f2f9be
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-29 10:42:05 +0530

    new commit🚀

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -2,27 +2,33 @@ function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-function diceGameSimulation(numSim) {
-  const res = [];
+// Function to simulate the dice game for a specified number of simulations
+function diceGameSimulation(numSimulations) {
+  const results = []; // Initialize an array to store simulation results
 
-  for (let i = 0; i < numSim; i++) {
-    const dice1 = rollDice();
-    const dice2 = rollDice();
-    const sum = dice1 + dice2;
+  // Loop through the specified number of simulations
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = rollDice(); // Simulate the roll of the first dice
+    const dice2 = rollDice(); // Simulate the roll of the second dice
+    const sum = dice1 + dice2; // Calculate the sum of the dice values
 
-    let res2 = "";
+    let result = ""; // Initialize a variable to store the result of the current simulation
 
+    // Check if the sum of the dice values is 7 or 11
     if (sum === 7 || sum === 11) {
-      res2 = "win";
+      result = "win"; // Set the result to 'win' if the sum is 7 or 11
     } else if (sum === 2 || sum === 3 || sum === 12) {
-      res2 = "lose";
+      result = "lose"; // Set the result to 'lose' if the sum is 2, 3, or 12
     } else {
-      res2 = "roll again";
+      result = "roll again"; // Set the result to 'roll again' for any other sum
     }
-    res2.push({ dice1, dice2, sum, res });
+
+    // Add the simulation result to the results array
+    results.push({ dice1, dice2, sum, result });
   }
 
-  return res;
+  return results; // Return the array of simulation results
 }
 
-module.exports = diceGameSimulation;
+module.exports = diceGameSimulation; // Export the diceGameSimulation function
+``;
