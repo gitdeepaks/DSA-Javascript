ebb27b0a0689e907f5ca54a5cb0c9f98f0e6bcf6
gitdeepaks <deepaksankhyan92@outlook.com>
2024-03-08 19:45:36 +0530

    new commit🚀

--- 03-high-order-array-methods/01-simple-examples/simple-examples.js
+++ 03-high-order-array-methods/01-simple-examples/simple-examples.js
@@ -4,22 +4,43 @@ const numbers = [1, 2, 3, 4, 5];
  * map: Transforms array elements with a provided function, creating a new array.
  */
 
+// const number = [1, 2, 3, 4, 5];
 
-/**
- * filter: Creates a new array with elements that satisfy a specified condition.
- */
+// const dubNumbers = numbers.map((n, i, arr) => {
+//   console.log(i);
+//   console.log(arr);
+//   return n * 2;
+// });
+
+// console.log(dubNumbers);
+
+// /**
+//  * filter: Creates a new array with elements that satisfy a specified condition.
+//  */
+
+// const evenNum = numbers.filter((n) => {
+//   return n % 2 === 0;
+// });
+
+// console.log(evenNum);
+
+// /**
+//  * reduce: Accumulates array elements into a single value using a provided function.
+//  */
+
+// const sum = numbers.reduce((acc, number) => {
+//   return acc + number;
+// });
+
+// console.log(sum);
+
+// /**
+//  * forEach: Iterates through array elements and applies a function without creating a new array.
 
 
-
-/**
- * reduce: Accumulates array elements into a single value using a provided function.
- */
-
-
-
-/**
- * forEach: Iterates through array elements and applies a function without creating a new array.
- */
+// numbers.forEach((element) => {
+//   console.log(element);
+// });
 
 
 
@@ -28,14 +49,24 @@ const numbers = [1, 2, 3, 4, 5];
  */
 
 
+ const fundNum  = numbers.find((num) => num > 2)
+//  console.log(fundNum);
+
 /**
  * some: Checks if at least one array element satisfies a condition.
  */
 
+const hasEvenNumber = numbers.some((num) => num %2 === 0)
+
+// console.log(hasEvenNumber)
  
 
 /**
  * every: Checks if all array elements satisfy a condition.
  */
+
+const allNumsgreThn0 = numbers.every((num) =>  num > 0)
+
+console.log(allNumsgreThn0)
 
 
