4b6c0bdb53e37a7b7f59a87349fb5bd7ae7cc988
gitdeepaks <deepaksankhyan92@outlook.com>
2024-03-08 19:51:47 +0530

    new commit🚀

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,12 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(nums) {
+  const evenSqrs = nums
+    .filter((n) => n % 2)
+    .map((n) => n ** 2)
+    .reduce((acc, sq) => acc + sq, 0);
+
+  return evenSqrs;
+
+  return;
+}
 
 module.exports = sumOfEvenSquares;
