5ff2e15444e34d877ee039140b24811a082d40e0
gitdeepaks <deepaksankhyan92@outlook.com>
2024-03-10 17:37:41 +0530

    new commit🚀

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,14 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(prods, taxRt) {
+  const totalSales = prods.reduce(
+    (acc, prod) => acc + prod.price * prod.quantity,
+    0
+  );
+
+  const taxAmount = (totalSales * taxRt) / 100;
+
+  const totalSalesWithTax = totalSales + taxAmount;
+
+  return parseFloat(totalSales.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
