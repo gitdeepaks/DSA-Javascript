935ea1bd3cd0b1fbe13b6816fbb88d5305fba62f
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-01 21:00:25 +0530

    new commit🚀

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -25,18 +25,29 @@
 
 function highestScoringWord(str) {
   const words = str.split(" ");
-
   const scores = words.map((word) => {
     return Array.from(word).reduce(
-      (score, ltr) => score + ltr.charCodeAt(0) - 96,
+      (score, letter) => score + letter.charCodeAt(0) - 96,
       0
     );
   });
 
-  const heightScore = Math.max(...scores);
-  const heighstIndex = scores.indexOf(heightScore);
+  const highestScore = Math.max(...scores);
+  const heighstIndex = scores.indexOf(highestScore);
 
   return words[heighstIndex];
+
+  // let highestScore = 0;
+
+  // let heighstIndex = 0;
+
+  // for (let index = 0; index < scores.length; index++) {
+  //   if (scores[index] > highestScore) {
+  //     heightScore = scores[index];
+  //     heighstIndex = index;
+  //   }
+  // }
+  // return words[heighstIndex];
 }
 
 module.exports = highestScoringWord;
