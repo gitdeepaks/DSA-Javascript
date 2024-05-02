52cdf58e173b5c56e02879ca04c787f9d4fd8409
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-02 17:20:18 +0530

    new commit🚀

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,18 @@
-function validatePassword() {}
+function validatePassword(password) {
+  const isLenthisValid = password.length >= 8;
+  const hasUpperCase = password
+    .split("")
+    .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+
+  const hasLowerCase = password
+    .split("")
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+
+  const hasDigit = password
+    .split("")
+    .some((char) => !isNaN(parseInt(char, 10)));
+
+  return isLenthisValid && hasUpperCase && hasLowerCase && hasDigit;
+}
 
 module.exports = validatePassword;
