122a760c62d66bbc7dbaa2d1dc8c6dd5bccbfeca
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-02 16:01:14 +0530

    new commit🚀

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,13 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (input) => {
+  const octects = input.split(".");
+  if (octects.length !== 4) {
+    return false;
+  }
+
+  return octects.every((octet) => {
+    const num = parseInt(octet);
+    return num >= 0 && num <= 255 && octet === num.toString();
+  });
+};
 
 module.exports = isValidIPv4;
