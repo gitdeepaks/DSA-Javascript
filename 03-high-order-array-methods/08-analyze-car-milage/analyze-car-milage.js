2250139c312d9245f3d39dd99fbb28df743acd69
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-02 17:13:46 +0530

    new commit🚀

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,23 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMilag = cars.reduce((sum, car) => sum + car.mileage, 0);
+
+  const avgMileage = totalMilag / cars.length;
+
+  const highestMilage = cars.reduce(
+    (highest, car) => (car.milage > highest.milage ? car : highest),
+    cars[0]
+  );
+  const lowestMilage = cars.reduce(
+    (highest, car) => (car.milage < lowest.milage ? car : lowest),
+    cars[0]
+  );
+
+  return {
+    averageMileage: parseFloat(averageMileage.toFixed(2)),
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
