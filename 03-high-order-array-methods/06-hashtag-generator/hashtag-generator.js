e2edf92b36be15b637e88aa6b96a63e42839d029
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-02 15:50:45 +0530

    new commit🚀

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,20 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  //   if (str.trim() === "") {
+  //     return false;
+  //   }
+
+  //   const words = str.trim().split(/\s+/);
+  // }
+  // return (capitalizeWords = words.map(
+  //   (word) => word.charAt(0).toUpperCase() + word.slice(1)
+  // ));
+  // const hasgTag = "#" + capitalizeWords.join("");
+  // return hasgTag.length > 140 ? false : hasgTag;
+  const hasgTag = str.split(" ").reduce((tag, word) => {
+    return tag + word.charAt(0).toUpperCase() + word.substring(1);
+  }, "#");
+
+  return hasgTag.length === 1 || hasgTag.length > 140 ? false : hasgTag;
+}
 
 module.exports = generateHashtag;
