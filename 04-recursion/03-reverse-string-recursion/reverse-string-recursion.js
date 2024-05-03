5753f4e8cf7ddd9483dd13e8ddcdd42fc11930ec
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-03 11:28:03 +0530

    new commit🚀

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+const reverseString = (str) =>
+  str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
+//   if (str === "") {
+//     return "";
+//   }
+//   console.log(str.substr(1));
+//   return reverseString(str.substr(1)) + str.charAt(0);
 
 module.exports = reverseString;
