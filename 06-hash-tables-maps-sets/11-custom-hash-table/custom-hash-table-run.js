621dc86fc69776f7f69fd5891803541602811f03
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-22 19:13:22 +0530

    new commit🚀

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,11 @@
-const HashTable = require('./custom-hash-table');
+const HashTable = require("./custom-hash-table");
+
+const myHashTable = new HashTable();
+
+myHashTable.set("John", "9460-44-66-94");
+myHashTable.set("Antonio", "9480-48-66-94");
+myHashTable.set("Hitesh", "9680-48-66-96");
+
+myHashTable.printTable();
+
+// console.log(result);
