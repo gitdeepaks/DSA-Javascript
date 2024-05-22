cb83d9ac26b346d9ef68559e01bf68cdf560dc21
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-22 17:43:01 +0530

    new commit🚀

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,24 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(nums) {
+  const numset = new Set(nums);
+
+  let logestSequesnce = 0;
+
+  for (const num of numset) {
+    if (!numset.has(num - 1)) {
+      let currentNum = num;
+
+      let currentSequence = 1;
+
+      while (numset.has(currentNum + 1)) {
+        currentNum++;
+        currentSequence++;
+      }
+
+      logestSequesnce = Math.max(logestSequesnce, currentSequence);
+    }
+  }
+
+  return logestSequesnce;
+}
 
 module.exports = longestConsecutiveSequence;
