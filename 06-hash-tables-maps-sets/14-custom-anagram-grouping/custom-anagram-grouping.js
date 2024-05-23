6ec64354c4f061ecf222294c7da99a791587f374
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-23 16:31:41 +0530

    new commit 🚀

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,19 @@
-const HashTable = require('./HashTable');
+const HashTable = require("./HashTable");
 
-function anagramGrouping() {}
+function anagramGrouping(words) {
+  const anagramGroups = new HashTable();
+
+  for (const word of words) {
+    const sortedCharts = word.split("").sort().join("");
+    // console.log(sortedCharts);
+
+    if (anagramGroups.get(sortedCharts)) {
+      anagramGroups.get(sortedCharts).push(word);
+    } else {
+      anagramGroups.set(sortedCharts, [word]);
+    }
+  }
+  return anagramGroups.getValues();
+}
 
 module.exports = anagramGrouping;
