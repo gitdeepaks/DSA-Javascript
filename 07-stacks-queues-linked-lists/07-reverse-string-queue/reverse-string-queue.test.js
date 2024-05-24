2ab231aec179af8c671de8851edb358bfa8161de
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-24 20:39:14 +0530

    new commit 🚀

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.test.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.test.js
@@ -1,7 +1,7 @@
-const reverseStringQueue = require('./reverse-string-queue');
+const reverseStringQueue = require("./reverse-string-queue");
 
-test('Reversing a string', () => {
-  expect(reverseStringQueue('Hello')).toBe('olleH');
-  expect(reverseStringQueue('JavaScript')).toBe('tpircSavaJ');
-  expect(reverseStringQueue('12345')).toBe('54321');
+test("Reversing a string", () => {
+  expect(reverseStringQueue("Hello")).toBe("olleH");
+  expect(reverseStringQueue("JavaScript")).toBe("tpircSavaJ");
+  expect(reverseStringQueue("12345")).toBe("54321");
 });
