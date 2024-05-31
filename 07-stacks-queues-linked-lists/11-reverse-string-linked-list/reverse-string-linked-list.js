92619c2ec6a75fce072e22a1a25817163a0f28c9
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-31 21:07:05 +0530

    new commit 🚀

--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,21 @@
-const { LinkedList } = require('./linked-list');
+const { LinkedList } = require("./linked-list");
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+  const list = new LinkedList();
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    list.add(str[i]);
+  }
+  let revStr = "";
+
+  let curr = list.head;
+
+  while (curr !== null) {
+    revStr += curr.data;
+    curr = curr.next;
+  }
+
+  return revStr;
+}
 
 module.exports = reverseStringLinkedList;
