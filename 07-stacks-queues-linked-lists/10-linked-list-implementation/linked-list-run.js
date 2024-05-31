05355a6d9307cffe32eeb232940399a292c0a60e
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-31 21:02:07 +0530

    new commit 🚀

--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
@@ -1,1 +1,16 @@
-const { LinkedList } = require('./linked-list');
+const { LinkedList } = require("./linked-list");
+
+const list = new LinkedList();
+
+list.add(100);
+list.add(200);
+list.add(300);
+list.add(400);
+
+list.insertAt(0, "a");
+list.removeFrom(1);
+
+// console.log(list.get(2));
+// console.log(list.get(0));
+// console.log(list.get(1));
+list.printAll();
