1163a8613be46d27b384b5775621b8d16c9c1f8e
gitdeepaks <deepaksankhyan92@outlook.com>
2024-06-02 16:56:30 +0530

    new commit 🚀

--- 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
+++ 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
@@ -1,5 +1,18 @@
-const DoublyLinkedList = require('./DoublyLinkedList');
+const DoublyLinkedList = require("./DoublyLinkedList");
 
-function findPairSum() {}
+function findPairSum(nums, targetSum) {
+  const seen = new DoublyLinkedList();
+
+  for (const num of nums) {
+    const diff = targetSum - num;
+
+    if (seen.contains(diff)) {
+      return [diff, num];
+    }
+    seen.append(num);
+  }
+
+  return null;
+}
 
 module.exports = findPairSum;
