6c5108976f39a857a22bbd29c325902558fe1146
gitdeepaks <deepaksankhyan92@outlook.com>
2024-09-26 19:28:48 +0530

    new commit 🚀

--- 08-binary-trees-graphs/12-graph-implementation/graph.js
+++ 08-binary-trees-graphs/12-graph-implementation/graph.js
@@ -4,12 +4,34 @@ class Graph {
   }
 
   addVertex(vertex) {
-    this.adjacencyList[vertex] = [];
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
   }
   addEdge(vertex1, vertex2) {
+    this.addVertex(vertex1);
+    this.addVertex(vertex2);
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
   printADjacencyList() {
     for (const vertex in this.adjacencyList) {
       console.log(`${vertex} -> ${this.adjacencyList[vertex].join(",")}`);
