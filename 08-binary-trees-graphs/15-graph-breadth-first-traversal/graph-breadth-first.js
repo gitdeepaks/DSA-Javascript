c64a7072f8720479e72d4fe6ff96e9612e288a65
gitdeepaks <deepaksankhyan92@outlook.com>
2024-09-26 20:02:50 +0530

    new commit 🚀

--- 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
+++ 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
@@ -1,5 +1,28 @@
-const Queue = require('./queue');
+const Queue = require("./queue");
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(graph, startingVertex) {
+  const visited = new Set();
+  const result = [];
+
+  const queue = new Queue();
+
+  queue.enqueue(startingVertex);
+
+  visited.add(startingVertex);
+
+  while (!queue.isEmpty()) {
+    const currentVertex = queue.dequeue();
+    result.push(currentVertex);
+
+    for (const neighbour of graph.adjacencyList[currentVertex]) {
+      if (!visited.has(neighbour)) {
+        queue.enqueue(neighbour);
+
+        visited.add(neighbour);
+      }
+    }
+  }
+  return result;
+}
 
 module.exports = breadthFirstTraversal;
