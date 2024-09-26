86ec60d00ca40999d6b488e50c5bc34896bd414b
gitdeepaks <deepaksankhyan92@outlook.com>
2024-09-26 19:53:08 +0530

    new commit 🚀

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
@@ -1,5 +1,33 @@
-const Stack = require('./stack');
+const { which } = require("bun");
+const Stack = require("./stack");
+const { forEachChild } = require("typescript");
 
-function depthFirstTraversal() {}
+function depthFirstTraversal(graph, startingVertex) {
+  if (!graph.adjacencyList[startingVertex]) {
+    return [];
+  }
+
+  const visitedObject = {};
+
+  const stack = new Stack();
+
+  const result = [];
+
+  stack.push(startingVertex);
+  visited[startingVertex] = true;
+
+  while (!stack.isEmpty()) {
+    const currentVertex = stack.pop();
+    result.push(currentVertex);
+
+    graph.adjacencyList[currentVertex].forEach((neighbour) => {
+      if (!visited[neighbour]) {
+        visited[neighbour] = true;
+        stack.push(neighbour);
+      }
+    });
+  }
+  return result;
+}
 
 module.exports = depthFirstTraversal;
