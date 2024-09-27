36098084d48673a1e8f3c4366260811929ef8db3
gitdeepaks <deepaksankhyan92@outlook.com>
2024-09-27 17:41:27 +0530

    new commit 🚀

--- 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
+++ 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
@@ -1,3 +1,14 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const currentElement = arr[i];
+    let j = 1 - i;
+    while (j >= 0 && arr[j] > currentElement) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = currentElement;
+  }
+  return arr;
+}
 
 module.exports = insertionSort;
